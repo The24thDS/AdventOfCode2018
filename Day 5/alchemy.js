9e651c8e9ca9eed1f95e902b2afe8c879232307c
David Sima <davidsima@windowslive.com>
2018-12-08 02:26:51 +0200

    Day 5 Part 2 JavaScript

--- Day 5/alchemy.js
+++ Day 5/alchemy.js
@@ -1,5 +1,5 @@
-let input = require("./input.js").input;
-// let input = "dabAcCaCBAcCcaDAdabAcCaCBAcCcaDAdabAcCaCBAcCcaDA";
+const inputData = require("./input.js").input;
+let input = inputData;
 const log = require("../global/format.js").indentedLog;
 String.prototype.reverse = function(){
     const split = this.split("");
@@ -7,10 +7,10 @@ String.prototype.reverse = function(){
     const string = reversed.join("");
     return string;
 }
-const React = (string) => {
+const React = (string, flags) => {
     let changes = false;
     while(1){
-        let regex = new RegExp(string, `g`);
+        let regex = new RegExp(string, flags);
         const oldLength = input.length;
         input = input.replace(regex, "");
         const newLength = input.length;
@@ -22,10 +22,22 @@ const firstPart = () => {
     for(let char = 97; char<123;)
     {
         const string = `${String.fromCharCode(char)}${String.fromCharCode(char).toUpperCase()}`;
-        if(React(string)) {char=97; continue;}
-        if(React(string.reverse())) {char=97; continue;}
+        if(React(string, `g`)) {char=97; continue;}
+        if(React(string.reverse(), `g`)) {char=97; continue;}
         char++;
     }
-    log("Dimension after reaction: ", input.length);
+    return input.length;
 }
-firstPart()
+const secondPart = () => {
+    let min=inputData.length;
+    for(let char = 97; char<123; char++)
+    {
+        input = inputData;
+        React(String.fromCharCode(char), `gi`);
+        const length = firstPart();
+        if(length<min) min=length;
+    }
+    return min;
+}
+log("Dimension after reaction: ", firstPart());
+log("Dimension after improved reaction: ", secondPart());
